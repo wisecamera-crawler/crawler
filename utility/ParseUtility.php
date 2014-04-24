30f047d441513a8892235f287cb2cb3f1489a209
Poyu Chen <poyu677@gmail.com>
2014-04-24 06:35:26 -0700

    move strIntToInt function of utility

---
+++ utility/ParseUtility.php
@@ -0,0 +1,14 @@
+<?
+class ParseUtility
+{
+    static public function intStrToInt($str){
+        $value = 0;
+        for($i = 0; $i < strlen($str); ++$i) {
+            if($str[$i] >= '0' AND $str[$i] <= '9') {
+                $value = $value*10 + ($str[$i] - '0');
+            }
+        }
+        return $value;
+    }
+}
+
