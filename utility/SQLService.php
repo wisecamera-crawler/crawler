c40932455ddc220b1c48b756a8fc61ab33072447
Poyu Chen <poyu677@gmail.com>
2014-05-20 09:43:15 +0800

    add comments, and add setupDBConnection function for timeout problem

--- utility/SQLService.php
+++ utility/SQLService.php
@@ -1,39 +1,105 @@
 <?php
+/**
+ * SQLService.php : Provide the APIs for crawler write data to db
+ *
+ * PHP version 5
+ *
+ * LICENSE : none
+ *
+ * @dependency DTO.php
+ * @author   Poyu Chen <poyu677@gmail.com>
+ */
+
 namespace wisecamera;
 
 //require_once "DTO.php";
 
 use \PDO;
 
+/**
+ * SQLService provides the APIs for crawler write data to db.
+ *
+ * On construction, it will initialize the DB connection,
+ * and when destructing, it will write the summary to DB for this connection.
+ * Before using this class,
+ * there are 3 variables you MUST configure:
+ *  SQLService::ip          IP of the DB
+ *  SQLService::user        User name of DB
+ *  SQLService::password    Password of DB
+ *
+ * LICENSE : none
+ *
+ * @author   Poyu Chen <poyu677@gmail.com>
+ */
 class SQLService
 {
+    /**
+     * This 3 variables are DB config,
+     *  ip          ip of DB
+     *  user        username of DB
+     *  password    password of DB
+     */
     public static $ip = "";
     public static $user = "";
     public static $password = "";
 
+    /**
+     * The DB connection object(PDO)
+     */
     private $connection;
+
+    /**
+     * The project id which this serviece so with
+     */
     private $projectId;
+
+    /**
+     * The proxy ip of this connecyion
+     */
     private $proxy;
 
+    /**
+     * Start time and end time of this job
+     */
     private $startTime;
     private $endTime;
+
+    /**
+     * States of diffrent target
+     * States are presented in string   "no_change"         get data but same as last time
+     *                                  "success_update"    get data and same as last time
+     *                                  "can_not_resolve"   data in web page can not be resolved
+     *                                  "cannot_get_data"   connection error
+     * may refer to DB schema for further information
+     */
     private $wikiState;
     private $vcsState;
     private $issueState;
     private $downloadState;
+
+    /**
+     * lastData, the latest data of this project, used to compared if data changed
+     */
     private $lastData;
 
+    /**
+     * constructor
+     *
+     * The construtor, set up states and get lastData
+     *
+     * @param string $projectId Project's id
+     * @param string $proxy     The proxy used this time
+     *
+     * @return none
+     */
     public function __construct($projectId, $proxy = "127.0.0.1")
     {
-        $dsn = "mysql:host=" . SQLService::$ip . ";dbname=NSC";
-        $this->connection = new PDO($dsn, SQLService::$user, SQLService::$password);
+        $this->setupDBConnection();
         $this->projectId = $projectId;
         $this->wikiState = "no_change";
         $this->vcsState = "no_change";
         $this->issueState = "no_change";
         $this->downloadState = "no_change";
-        $this->connection->query("SET CHARACTER SET utf8 ");
-        $this->connection->query("SET NAMES utf8");
         $result = $this->connection->query(
             "SELECT * FROM `project` WHERE `project_id` = '$projectId'"
         );
@@ -43,14 +109,30 @@ class SQLService
         $this->proxy = $proxy;
     }
 
+    /**
+     * destructor
+     *
+     * To writeback summary when finished
+     */
     public function __destruct()
     {
         $this->endTime = date("Y-m-d H:i:s");
         $this->writeSummaray();
     }
 
+    /**
+     * insertIssue
+     *
+     * Insert the issue data into DB, and checl if data changed
+     *
+     * @param Issue $issue the inserted Issue object
+     *
+     * @return none
+     */
     public function insertIssue(Issue $issue)
     {
+        $this->setupDBConnection();
+
         if ($this->lastData["issue_topic"] != $issue->topic
             or $this->lastData["issue_post"] != $issue->article
             or $this->lastData["issue_user"] != $issue->account
@@ -74,8 +156,19 @@ class SQLService
         );
     }
 
+    /**
+     * insertWiki
+     *
+     * Insert the wiki data into DB, and checl if data is changed
+     *
+     * @param Wiki $wiki The inserted Wiki object
+     *
+     * @return none
+     */
     public function insertWiki(Wiki $wiki)
     {
+        $this->setupDBConnection();
+
         if ($this->lastData["wiki_pages"] != $wiki->pages or
             $this->lastData["wiki_line"] != $wiki->line or
             $this->lastData["wiki_update"] != $wiki->update) {
@@ -97,8 +190,19 @@ class SQLService
 
     }
 
+    /**
+     * insertVCS
+     *
+     * insert the vcs data into DB, and check if data checnged
+     *
+     * @param VCS $vcs Inserted VCS object
+     *
+     * @return none
+     */
     public function insertVCS(VCS $vcs)
     {
+        $this->setupDBConnection();
+
         if ($this->lastData["vcs_commit"] != $vcs->commit or
             $this->lastData["vcs_size"] != $vcs->size or
             $this->lastData["vcs_line"] != $vcs->line or
@@ -124,8 +228,20 @@ class SQLService
 
     }
 
+    /**
+     * insertDownload
+     *
+     * Insert the download data into DB,
+     * and sum up the total download files, counts, and check if data changed
+     *
+     * @param Array $downloads Array of Download object
+     *
+     * @return none
+     */
     public function insertDownload(array $downloads)
     {
+        $this->setupDBConnection();
+
         $valueString = "VALUES";
         $dlCount = 0;
         $dlFile = 0;
@@ -155,8 +271,19 @@ class SQLService
         );
     }
 
+    /**
+     * insertVCSCommiters
+     *
+     * insert the VCSCommters data into DB
+     *
+     * @param Array $commiters array of VCSCommiter
+     *
+     * @return none
+     */
     public function insertVCSCommiters(array $commiters)
     {
+        $this->setupDBConnection();
+
         $valueString = "VALUES";
         foreach ($commiters as $commiter) {
             $valueString .= "('$commiter->commiter', '$this->projectId',
@@ -170,8 +297,21 @@ class SQLService
         );
     }
 
+    /**
+     * insertWikiPage
+     *
+     * insert the each wiki page data into DB
+     * The diffrent of WikiPage and Wiki is that WikiPage is data of single wiki page,
+     * and Wiki is the summary of assigned projects' wiki
+     *
+     * @param Array $pages array of WikiPage object
+     *
+     * @return none
+     */
     public function insertWikiPages(array $pages)
     {
+        $this->setupDBConnection();
+
         $valueString = "VALUES";
         foreach ($pages as $page) {
             $valueString .= "('$page->url', '$this->projectId', '$page->title',
@@ -185,8 +325,19 @@ class SQLService
         );
     }
 
+    /**
+     * insertRating
+     *
+     * insert the rating data into DB
+     *
+     * @param Rating $rating
+     *
+     * @return none
+     */
     public function insertRating(Rating $ranking)
     {
+        $this->setupDBConnection();
+
         $this->connection->query(
             "UPDATE `project`
                 SET `star` = '$ranking->star',
@@ -201,16 +352,35 @@ class SQLService
         );
     }
 
+    /**
+     * getProjectInfo
+     *
+     * Get the data of this project
+     * The detail may refer to DB schema
+     *
+     * @param string $type The target info want to get (field name of project table)
+     *
+     * @return string The assigned information
+     */
     public function getProjectInfo($type)
     {
         return $this->lastData[$type];
     }
 
+    /**
+     * writeSummary
+     *
+     * Write the summary of this crawling result.
+     * In other words, write states of each target into DB (table crawl_status)
+     * Automatically invoked on destruction
+     */
     private function writeSummaray()
     {
         if ($this->lastData == null) {
             return;
         }
+
+        $this->setupDBConnection();
 
         $arr = array($this->wikiState, $this->vcsState,
             $this->issueState, $this->downloadState);
@@ -236,5 +406,18 @@ class SQLService
                 '$this->issueState', '$this->downloadState', '$ip',
                 '$this->startTime', '$this->endTime')"
         );
+    }
+
+    /**
+     * setupDBConnection
+     *
+     * set up the DB connection (due to long crawling time, DB connection may time out)
+     */
+    private function setupDBConnection()
+    {
+        $dsn = "mysql:host=" . SQLService::$ip . ";dbname=NSC";
+        $this->connection = new PDO($dsn, SQLService::$user, SQLService::$password);
+        $this->connection->query("SET CHARACTER SET utf8 ");
+        $this->connection->query("SET NAMES utf8");
     }
 }
